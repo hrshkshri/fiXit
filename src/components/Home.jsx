60c01bae89d2598d11d7ea04e0b9e1eee712280a
hrshkshri <harshkeshari100@gmail.com>
2023-12-20 00:43:43 +0530

    heading style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,47 @@
 import { Box, Center, Heading } from '@chakra-ui/react';
 import Chatbotkit from './Chatbot/Chatbotkit';
+import './heading.css';
 
 const Home = ({ isLoggedIn }) => {
-
-
   return (
     <>
+      <style>
+        {`
+          @keyframes text-reveal {
+            from {
+              opacity: 0;
+              transform: translateY(20px);
+            }
+            to {
+              opacity: 1;
+              transform: translateY(0);
+            }
+          }
+
+          .text-reveal-animation {
+            animation: text-reveal 4s ease 0.72s 1 normal forwards running;
+          }
+
+          .heading1 {
+            font-size: 3em;
+          }
+
+          .heading2 {
+            font-size: 4em;
+          }
+
+          @media (max-width: 768px) {
+            .heading1 {
+              font-size: 2em; /* Adjust as needed for smaller screens */
+            }
+
+            .heading2 {
+              font-size: 3em; /* Adjust as needed for smaller screens */
+            }
+          }
+        `}
+      </style>
+
       <Box
         bg="#212520"
         color="grey"
@@ -17,9 +53,18 @@ const Home = ({ isLoggedIn }) => {
         alignItems="center"
       >
         <Center>
-          <div>
-            <Heading className='text-6xl'>Fix your Sales</Heading>
-            <Heading className='text-5xl' mt="4">Enabling Human Intelligence with Artificial Intelligence</Heading>
+          <div className='flex p-0 md:pt-36 px-10 lg:p-0 pb-0 flex-col text-center text-white items-center gap-8 sm:-mt-[2%] justify-between'>
+            <Heading
+              className='heading1 overflow-hidden leading-6 h-full font-semibold font-proximaBold -mt-[2%] xl:mt-10 bg-clip-text text-[#c4dffe] text-reveal-animation'
+            >
+              Fix your Sales
+            </Heading>
+            <Heading
+              className='heading2 w-full leading-tight md:mt-5 p-0 text-center font-proximaBold -mt-[2%] bg-clip-text text-[#c4dffe] text-reveal-animation'
+              mt="4"
+            >
+              Human Intelligence with Enabling Artificial Intelligence
+            </Heading>
           </div>
         </Center>
       </Box>
